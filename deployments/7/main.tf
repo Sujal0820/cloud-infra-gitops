cd7bd662608f3d87edf2084992ef3311e9666e6e
Sujal0820 <sujaljamsandekar@gmail.com>
2025-12-11 20:21:15 +0530

    Deployment 7: Add generated terraform

---
+++ deployments/7/main.tf
@@ -0,0 +1,13 @@
+
+resource "aws_instance" "example" {
+  ami = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my-bucket-7"
+}
